14b4a5db08cb699e676018aa18502753e95ad720
Adelaja Qowiyyu <adexolamilekan46@gmail.com>
2023-09-14 18:00:12 +0100

    fixed type error in  main.pyu

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ async def create_person(person: schemas.PersonBase, db: db_dependency):
 
     
 @app.get('/api/{user}')
-async def get_person(db: db_dependency, user: int | str = Path(title="The ID or name of the user")):
+async def get_person(db: db_dependency, user):
     crud.get_person(db=db, user=user)
 
 @app.patch("/api/{user}")
-async def update_person(updated_person: schemas.PersonBase,   db: db_dependency, user:int |str = Path(title="The ID or name of the user")):
+async def update_person(updated_person: schemas.PersonBase,   db: db_dependency, user):
     crud.update_person(updated_person=updated_person, db=db, user=user)
 
 @app.delete("/api/{user}")
-async def delete_person(db:db_dependency, user:int |str = Path(title="The ID or name of the user")):
+async def delete_person(db:db_dependency, user):
     crud.delete_person(db=db, user=user)
